785f605a4fe1a20d128560f3d9b611295437e4a1
ttl <thetraderslight@gmail.com>
2025-11-09 13:55:47 -0500

    init

--- src/routes.php
+++ src/routes.php
@@ -1,1 +1,10 @@
 // Placeholder for additional routes.
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+$app->get('/', function (Request $request, Response $response) {
+    $html = file_get_contents(__DIR__ . '/../public/home.html');
+    $response->getBody()->write($html);
+    return $response;
+});
